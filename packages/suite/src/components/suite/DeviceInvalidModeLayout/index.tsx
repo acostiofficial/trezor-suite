31a6fdbac317021adbfad739dfee4ccb59fd085e
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-08-18 11:56:55 +0100

    chore(suite-desktop): use device selectors

--- packages/suite/src/components/suite/DeviceInvalidModeLayout/index.tsx
+++ packages/suite/src/components/suite/DeviceInvalidModeLayout/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import styled, { css } from 'styled-components';
+
 import { Button, Image, ImageType } from '@trezor/components';
+
 import { Translation } from 'src/components/suite';
 import { useDispatch, useSelector } from 'src/hooks/suite';
 import { goto } from 'src/actions/suite/routerActions';
+
 import { Modal } from '../Modal';
+import { selectDevicesCount } from '../../../reducers/suite/deviceReducer';
 
 const StyledImage = styled(Image)<{ image: ImageType }>`
     flex: 1;
@@ -50,7 +55,7 @@ const DeviceInvalidModeLayout = ({
     resolveButton,
     'data-test': dataTest,
 }: DeviceInvalidModeLayoutProps) => {
-    const devices = useSelector(state => state.devices);
+    const devicesCount = useSelector(selectDevicesCount);
     const dispatch = useDispatch();
 
     const handleSwitchDeviceButtonClick = () =>
@@ -64,7 +69,7 @@ const DeviceInvalidModeLayout = ({
             bottomBar={
                 <>
                     {resolveButton && resolveButton}
-                    {allowSwitchDevice && devices.length > 1 && (
+                    {allowSwitchDevice && devicesCount > 1 && (
                         <Button onClick={handleSwitchDeviceButtonClick}>
                             <Translation id="TR_SWITCH_DEVICE" />
                         </Button>
